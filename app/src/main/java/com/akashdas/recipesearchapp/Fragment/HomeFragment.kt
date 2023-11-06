5dc947ffafcee1702a8523e821c1b24f21e89307
Akash Das <akashdas.dev@gmail.com>
2023-11-06 16:51:02 +0600

    home xml complete

--- app/src/main/java/com/akashdas/recipesearchapp/Fragment/HomeFragment.kt
+++ app/src/main/java/com/akashdas/recipesearchapp/Fragment/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.akashdas.recipesearchapp.Fragment
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
+import android.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.akashdas.recipesearchapp.Helper.Singleton
 import com.akashdas.recipesearchapp.R
 import com.akashdas.recipesearchapp.databinding.FragmentHomeBinding
@@ -26,16 +31,44 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // all about chip
+        chip()
+
+    }
+
+    private fun chip() {
         binding.chipGroup.isSingleSelection = true
+
         Singleton.chipTexts.forEach { text ->
-            val chip = Chip(requireContext())
-            chip.text = text
-            chip.isCheckable = true
+            val chip = Chip(context).apply {
+                this.text = text
+                isCheckable = true
+                // Define color in your colors.xml
+                chipStrokeColor = ColorStateList
+                    .valueOf(ContextCompat.getColor(context, R.color.chip_color))
+            }
             binding.chipGroup.addView(chip)
         }
 
+        // select the first chip by default
+        (binding.chipGroup.getChildAt(0) as? Chip)?.isChecked = true
+
         binding.chipGroup.setOnCheckedChangeListener { group, checkedId ->
-            // Handle the checked chip change if needed
+            // The 'checkedId' is the ID of the selected chip.
+            val chip = group.findViewById<Chip>(checkedId)
+            chip?.let {
+                val selectedText: String = it.text.toString()
+
+                binding.editTextText.setText(selectedText)
+
+                searchData(selectedText)
+            }
+
         }
+
+    }
+
+    private fun searchData(selectedText: String) {
+
     }
 }
